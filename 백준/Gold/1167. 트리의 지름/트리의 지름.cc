d3e168fbc35fd0011d7e3f07b5deb7e4ce560b9c
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-04-28 20:15:10 +0900

    [Gold II] Title: 트리의 지름, Time: 92 ms, Memory: 8944 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+vector<vector<pair<int, int> > > v;
+int visited[100001] = {0};
+
+void dfs(int n) {
+    for (int i = 0; i < v[n].size(); i++) {
+        if (visited[v[n][i].first] == 0) {
+            visited[v[n][i].first] = visited[n] + v[n][i].second;
+            dfs(v[n][i].first);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int V;
+    cin >> V;
+    v.resize(V + 1);
+    for (int i = 0; i < V; i++) {
+        int thisnode;
+        int nextnode, dist, temp;
+        int cnt = 0;
+        cin >> thisnode;
+        cin >> temp;
+        while (temp != -1) {
+            if (cnt % 2 == 0) {
+                nextnode = temp;
+            }
+            else {
+                dist = temp;
+                v[thisnode].push_back(make_pair(nextnode, dist));
+            }
+            cnt++;
+            cin >> temp;
+        }
+
+    }
+
+
+    visited[1] = 1;
+    dfs(1);
+    int maxNode = 0;
+    for (int i = 1; i <= V; i++) {
+        if (visited[i] > visited[maxNode]) {
+            maxNode = i;
+        }
+    }
+    memset(visited, 0, sizeof(visited));
+    visited[maxNode] = 1;
+    dfs(maxNode);
+    sort(visited, visited + V + 1);
+    cout << visited[V] - 1;
+
+
+
+}
