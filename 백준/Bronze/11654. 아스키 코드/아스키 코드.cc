de06de18e82f01abb35294df61b0aa10e62c1062
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-04-28 20:36:51 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    char c;
+    cin >> c;
+    cout << (int)c;
+
+}
