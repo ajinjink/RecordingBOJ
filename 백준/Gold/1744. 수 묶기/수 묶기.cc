568a4af35d0e21dae4afec322a3c6df1470f8b81
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-04-28 20:18:44 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,71 @@
+
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    priority_queue<int> pluspq; // descending
+    priority_queue<int, vector<int>, greater<int> > minuspq; // ascending
+    int one = 0, zero = 0;
+
+    for (int i = 0; i < N; i++) {
+        int data;
+        cin >> data;
+        if (data > 1) {
+            pluspq.push(data);
+        }
+        else if (data == 1) {
+            one++;
+        }
+        else if (data == 0) {
+            zero++;
+        }
+        else {
+            minuspq.push(data);
+        }
+
+
+
+    }
+    int sum = 0;
+
+    while (pluspq.size() > 1) {
+        int first = pluspq.top();
+        pluspq.pop();
+        int second = pluspq.top();
+        pluspq.pop();
+        sum += first * second;
+    }
+    if (!pluspq.empty()) {
+        sum += pluspq.top();
+        pluspq.pop();
+    }
+
+    while (minuspq.size() > 1) {
+        int first = minuspq.top();
+        minuspq.pop();
+        int second = minuspq.top();
+        minuspq.pop();
+        sum += first * second;
+    }
+    if (!minuspq.empty()) {
+        if (zero == 0) {
+            sum += minuspq.top();
+            minuspq.pop();
+        }
+        else {
+            minuspq.pop();
+        }
+    }
+
+    sum += one;
+
+    cout << sum;
+}
