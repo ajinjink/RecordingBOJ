11cbb7f641a3b7f42d4ababf9d0074ba951732ad
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-04-28 20:33:57 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string id;
+    cin >> id;
+    cout << id << "??!";
+
+}
