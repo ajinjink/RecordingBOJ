dd01c157136b1c710a2eb841fe40ba8e2fc25347
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-04-28 20:25:35 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+
+    int fibo[46];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    cout << fibo[n];
+
+}
