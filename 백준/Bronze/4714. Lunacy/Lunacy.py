2e79aaaf4d4badd0eec8e3eddd3512c74cffa473
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-08-01 19:18:24 +0900

    [Bronze IV] Title: Lunacy, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4714. Lunacy/Lunacy.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+# a, b = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+while True:
+    a = float(input())
+    if a < 0:
+        break
+    print(f"Objects weighing {a:.2f} on Earth will weigh {a * 0.167:.2f} on the moon.")
