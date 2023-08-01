925e6784b35fa650fc0a1aa38ba99c04210d2d69
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2023-08-01 19:13:50 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 80 ms, Memory: 4940 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int> > v(N + 1);
+    vector<int> indegree(N + 1);
+
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        indegree[b]++;
+    }
+
+    queue<int> q;
+    vector<int> res(N + 1);
+
+    for (int i = 1; i <= N; i++) {
+        if (!indegree[i]) {
+            q.push(i);
+            res[i] = 1;
+        }
+    }
+
+
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        
+        for (int i = 0; i < v[now].size(); i++) {
+            int next = v[now][i];
+            indegree[next]--;
+            if (indegree[next] == 0) {
+                q.push(next);
+                res[next] = res[now] + 1;
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << res[i] << ' ';
+    }
+}
