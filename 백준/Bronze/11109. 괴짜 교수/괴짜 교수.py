24b7e466ca4484a54bec72ea54f81fe2966f99c3
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:35:50 +0900

    [Bronze III] Title: 괴짜 교수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11109. 괴짜 교수/괴짜 교수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    d, n, s, p = map(int, input().split())
+    if d + p * n == n * s:
+        print("does not matter")
+    elif d + p * n > n * s:
+        print("do not parallelize")
+    else:
+        print("parallelize")
