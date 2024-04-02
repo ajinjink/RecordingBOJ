d6922e48b95765d3d8bfd1c7eb1615f5c460ed35
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:20:15 +0900

    [Bronze III] Title: More Dice, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9288. More Dice/More Dice.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(int(input())):
+    n = int(input())
+    print(f"Case {i+1}:")
+    for a in range(1, 7):
+        for b in range(a, 7):
+            if a+b == n:
+                print(f"({a},{b})")
