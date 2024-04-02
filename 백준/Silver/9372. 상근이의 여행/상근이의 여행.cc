041ab1c9fd1bf2e57325d83226188366b52aac47
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:21:26 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+#define pii pair<int, int>
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; 
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n - 1 << endl;
+
+    }
+    
+    
+}
