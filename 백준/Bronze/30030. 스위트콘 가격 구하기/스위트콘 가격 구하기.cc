0e0c4e3af208d2109374e21c2041c24c32101e8c
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:33:27 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+#define pii pair<int, int>
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int B;
+    cin >> B;
+    B /= 11;
+    B *= 10;
+    cout << B;
+    
+}
