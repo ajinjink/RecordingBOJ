ee236fc268a39d38c01f1be5168eb30e6d02b8c2
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 14:53:38 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+for _ in range(k):
+    if n // 2 >= m:
+        n -= 1
+    else:
+        m -= 1
+print(min(n // 2, m))
