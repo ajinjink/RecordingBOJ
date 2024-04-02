924b109a7a301e5df11a5287eea27a351895b114
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:16:41 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 136 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(int(input())):
+    n = int(input())
+    a, b = map(int, input().split())
+    for _ in range(n):
+        ui, vi = map(int, input().split())
+    print("Material Management", i + 1)
+    print("Classification ---- End!")
