99f98466aa99c7f58ebd40f6a279cecef76bb708
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:53:58 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9] = {0};
+
+void choice(int x, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = x; i <= n; i++) {
+		arr[cnt] = i;
+		choice(i, cnt + 1);
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	cin >> n >> m;
+
+	choice(1, 0);
+
+
+}
