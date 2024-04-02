2f35bbd4c1c53fd0549c478e2e8454de3f75e673
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:19:45 +0900

    [Bronze III] Title: 젓가락 게임, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25642. 젓가락 게임/젓가락 게임.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+while True:
+    b += a
+    if b >= 5:
+        print('yt')
+        break
+    a += b
+    if a >= 5:
+        print('yj')
+        break
