83f87d2ff18da4ed0431cf1d5ebbd5a3581eb71e
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:28:10 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+str = input().strip()
+
+if str == "NLCS":
+    print("North London Collegiate School")
+if str == "BHA":
+    print("Branksome Hall Asia")
+if str == "KIS":
+    print("Korea International School")
+if str == "SJA":
+    print("St. Johnsbury Academy")
