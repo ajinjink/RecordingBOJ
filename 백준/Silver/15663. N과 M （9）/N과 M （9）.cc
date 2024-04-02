c6b0b80a4e8f2eaf0b6c9113e714eff697590d08
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:54:16 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9] = {0};
+int num[9] = {0};
+int visited[9] = {0};
+
+void choice(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	int prev = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i] && num[i] != prev) {
+			arr[cnt] = num[i];
+			visited[i] = 1;
+			prev = arr[cnt];
+			choice(cnt + 1);
+			visited[i] = 0;
+		}
+	}
+}
+
+
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+
+	sort(num, num + n);
+
+	choice(0);
+
+
+}
