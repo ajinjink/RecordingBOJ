43dac3fbcc1950b98eb20ef2972a9d05c53c7c08
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:05:51 +0900

    [Bronze III] Title: J박스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5354. J박스/J박스.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+    if n < 3:
+        for _ in range(n):
+            print('#'*n)
+        print()
+    else:
+        print('#'*n)
+        for _ in range(n - 2):
+            print('#' + 'J'*(n - 2) + '#')
+        print('#' * n)
+        print()
