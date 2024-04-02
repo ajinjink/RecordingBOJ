45174b2342d1bd0ce3bab97bd522d7fd80b86dde
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:17:18 +0900

    [Bronze III] Title: 시간과 날짜, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25024. 시간과 날짜/시간과 날짜.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+l = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    
+    if 0 <= x <= 23 and 0 <= y <= 59:
+        print("Yes", end=" ")
+    else:
+        print("No", end=" ")
+
+    if 1 <= x <= 12:
+        if 1 <= y <= l[x]:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("No")
