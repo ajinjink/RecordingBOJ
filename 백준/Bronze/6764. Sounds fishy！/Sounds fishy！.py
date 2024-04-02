e1cb4e1c5c2e4c327f8775584b10fd3f1844322c
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:11:52 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a < b and b < c and c < d:
+    print("Fish Rising")
+elif a > b and b > c and c > d:
+    print("Fish Diving")
+elif a == b and b == c and c == d:
+    print("Fish At Constant Depth")
+else:
+    print("No Fish")
