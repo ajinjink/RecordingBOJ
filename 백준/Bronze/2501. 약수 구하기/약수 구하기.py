be20712fe193c2e7f61c3670fdcfce83518c3d07
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 14:47:01 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+l = []
+for i in range(1, a + 1):
+    if a % i == 0:
+        l.append(i)
+print(l[b - 1] if len(l) >= b else 0)
