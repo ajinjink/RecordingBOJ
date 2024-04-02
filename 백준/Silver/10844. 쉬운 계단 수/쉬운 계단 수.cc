486187cca2cbec8d269725d3525b721f309aeae4
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:31:42 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#define endl '\n'
+#define pii pair<int, int>
+using ll = long long;
+using namespace std;
+
+static int n;
+static long d[101][11];
+static long mod = 1000000000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+
+    for (int i = 1; i <= 9; i++) {
+        d[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        d[i][0] = d[i - 1][1];
+        d[i][9] = d[i - 1][8];
+        for (int j = 1; j <= 8; j++) {
+            d[i][j] = (d[i - 1][j - 1] + d[i - 1][j + 1]) % mod;
+        }
+    }
+
+    long sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        sum = (sum + d[n][i]) % mod;
+    }
+
+    cout << sum;
+}
