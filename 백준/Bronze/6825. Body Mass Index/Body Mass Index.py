291945d3a5bfe00b72dea17d49ef1b5c7e033595
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:12:48 +0900

    [Bronze IV] Title: Body Mass Index, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+w = float(input())
+h = float(input())
+bmi = w / (h * h)
+
+if bmi > 25:
+    print("Overweight")
+elif 18.5 <= bmi <= 25:
+    print("Normal weight")
+else:
+    print("Underweight")
+
