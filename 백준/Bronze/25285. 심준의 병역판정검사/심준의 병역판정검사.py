57a7d25ab9a2002992e93931e842f7d10c48076b
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:17:55 +0900

    [Bronze III] Title: 심준의 병역판정검사, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25285. 심준의 병역판정검사/심준의 병역판정검사.py
@@ -0,0 +1,23 @@
+for _ in range(int(input())):
+    h, w = map(int, input().split())
+    bmi = w / ((h / 100)**2)
+    if h < 140.1:
+        print(6)
+    elif 140.1 <= h < 146:
+        print(5)
+    elif 146 <= h < 159:
+        print(4)
+    elif 159 <= h < 161:
+        if 16 <= bmi < 35:
+            print(3)
+        else:
+            print(4)
+    elif 161 <= h < 204:
+        if 20 <= bmi < 25:
+            print(1)
+        elif (18.5 <= bmi < 20) or (25 <= bmi < 30):
+            print(2)
+        elif (16 <= bmi < 18.5) or (30 <= bmi < 35):
+            print(3)
+        else:
+            print(4)
