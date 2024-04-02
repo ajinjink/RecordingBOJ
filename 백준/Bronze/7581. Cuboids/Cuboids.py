d982cdc0d743e1017ebf30cbf20c88ab8774019f
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:15:08 +0900

    [Bronze III] Title: Cuboids, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    l, w, h, v = map(int, input().split())
+    if l == w == h == v == 0:
+        break
+    if l == 0:
+        l = (v // w // h)
+    elif w == 0:
+        w = (v // l //h)
+    elif h == 0:
+        h = (v // l // w)
+    elif v == 0:
+        v = (l * w * h)
+    print(l, w, h, v)
