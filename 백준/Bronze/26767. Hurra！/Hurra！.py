b86e6af9abacbf4d921ec1a7c6db52e8ce580438
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:24:44 +0900

    [Bronze IV] Title: Hurra!, Time: 728 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26767. Hurra！/Hurra！.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(1, n + 1):
+    if i % 7 == 0 and i % 11 != 0:
+        print("Hurra!")
+    elif i % 7 != 0 and i % 11 == 0:
+        print("Super!")
+    elif i % 7 == 0 and i % 11 == 0:
+        print("Wiwat!")
+    else:
+        print(i)
