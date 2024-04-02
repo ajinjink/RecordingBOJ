5045e426c95a03a66391ab4112cc83b1844f3fe0
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 15:13:41 +0900

    [Bronze III] Title: Trident, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6903. Trident/Trident.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+s = int(input())
+h = int(input())
+for _ in range(t):
+    print('*' + ' '*s + '*' + ' '*s + '*')
+print('*' * (3 + 2*s))
+for _ in range(h):
+    print(' ' * (s + 1) + '*')
