2ad1c09bd229be740db326e3b995fbf60259d5c7
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 14:47:51 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    input()
+    n = int(input())
+    sum = 0
+    for _ in range(n):
+        sum += int(input())
+    if sum % n == 0:
+        print("YES")
+    else:
+        print("NO")
