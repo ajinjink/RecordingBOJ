2e3c8e3930199b79ee6f6f8bcb670648ffabbee5
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 16:27:40 +0900

    [Bronze IV] Title: Sun and Moon, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27590. Sun and Moon/Sun and Moon.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+ds, ys = map(int, input().split())
+dm, ym = map(int, input().split())
+sun = ys - ds
+moon = ym - dm
+
+while sun != moon:
+    if sun < moon:
+        sun += ys
+    else:
+        moon += ym
+print(sun)
