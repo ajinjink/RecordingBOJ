5d4cf81da8e5beac691af2e0c9990937623c273e
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-02 14:32:24 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,31 +1,23 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+int dp[1000001] = {0};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
-    int N, count = 0;
+
+    int N;
     cin >> N;
 
-    int* dp = new int[N + 1];
-
     dp[1] = 0;
-    dp[2] = 1;
-
-    for (int i = 3; i <= N; i++) {
+    for (int i = 2; i <= N; i++) { // i에서 1까지 가는데 필요한 계산 횟수
         dp[i] = dp[i - 1] + 1;
-
-        if (i % 3 == 0) {
-            dp[i] = min(dp[i], dp[i/3] + 1);
-        }
-        
-        if (i % 2 == 0) {
-            dp[i] = min(dp[i], dp[i/2] + 1);
-        }
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
     }
 
     cout << dp[N];
-    
 }
