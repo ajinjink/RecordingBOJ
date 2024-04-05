01c03d110bb38f1851ec54b50c55582eb464a24c
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-04-05 13:26:20 +0900

    [Platinum III] Title: 열혈강호 4, Time: 720 ms, Memory: 6160 KB -BaekjoonHub

---
+++ 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, M, K;
+vector<int> g[2010]; // 직원 그래프
+vector<int> par(2010, -1);   // 일 그래프
+bool visited[2010]; // 일 그래프 visited
+
+int match(int v) { // v번 직원을 다른 일과 매칭할 수 있는가?
+    // if (visited[v]) return 0;
+    // visited[v] = true;
+
+    for (auto n : g[v]) { // v와 연결된 저쪽 그래프 노드들
+        if (visited[n]) continue;
+        visited[n] = 1;
+        if (par[n] == -1 || match(par[n])) { // 매칭 안 됐거나 다른 점과 매칭 가능
+            par[n] = v; // 연결
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> K;
+
+    for (int i = 1; i <= N; i++) {
+        int n;
+        cin >> n;
+
+        for (int j = 0; j < n; j++) {
+            int a;
+            cin >> a;
+            g[i].push_back(a);
+            g[1000 + i].push_back(a);
+        }
+    }
+
+    int res = 0;
+    for (int i = 1; i <= N; i++) {
+        fill(&visited[0], &visited[1011], 0);
+        if (match(i)) res++;
+    }
+    while (1) {
+        bool flag = false;
+        for (int i = 1; i <= N; i++) {
+            if (!K) break;
+            fill(&visited[0], &visited[1011], 0);
+            if (match(i)) {
+                K--;
+                res++;
+                flag = true;
+            }
+        }
+        if (!flag) break;
+    }
+  
+    cout << res;
+
+}
