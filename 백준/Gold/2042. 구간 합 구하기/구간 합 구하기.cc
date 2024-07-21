22ee20ede28c76eefb141adb4b370abeb6c4cd4a
Jina Kim <105297115+ajinjink@users.noreply.github.com>
2024-07-21 19:11:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 184 ms, Memory: 18596 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,82 +1,74 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <cmath>
+#define ll long long
 #define endl '\n'
-#define pii pair<int, int>
-using ll = long long;
 using namespace std;
 
 int N, M, K;
-vector<long> tree;
+vector<ll> v;
 
-void maketree(int i) {
-    while (i != 1) {
-        tree[i / 2] += tree[i];
-        i--;
+void init(ll index) {
+    while (index != 1) {
+        v[index / 2] += v[index];
+        index--;
     }
 }
 
-void change(int index, long val) { // 트리 값 변경
-    long diff = val - tree[index];
+void change(ll place, ll num) {
+    ll diff = v[place] - num;
 
-    while (index > 0) {
-        tree[index] = tree[index] + diff;
-        index /= 2;
+    while (place) {
+        v[place] -= diff;
+        place /= 2;
     }
 }
 
-long getsum(int s, int e) {
-    long sum = 0;
+ll sum(ll a, ll b) {
+    ll res = 0;
 
-    while (s <= e) {
-        if (s % 2 == 1) {
-            sum += tree[s];
-            s++;
+    while (a <= b) {
+        if (a % 2 == 1) {
+            res += v[a];
+            a++;
         }
-        if (e % 2 == 0) {
-            sum += tree[e];
-            e--;
+        if (b % 2 == 0) {
+            res += v[b];
+            b--;
         }
-        s /= 2;
-        e /= 2;
+        a /= 2;
+        b /= 2;
     }
 
-    return sum;
+    return res;
 }
-
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> N >> M >> K;
 
-    int height = 0; // 트리 높이 구하기
-    int len = N;
+    int h = 0, len = N;
     while (len) {
         len /= 2;
-        height++;
+        h++;
     }
-
-    int treesize = int(pow(2, height + 1)); // 원본배열을 리프 노드로 두는 트리 크기
-    int left = treesize / 2 - 1; // 원본 배열 저장 시작 인덱스
-    tree.resize(treesize + 1);
-
-    for (int i = left + 1; i <= left + N; i++) cin >> tree[i];
-    maketree(treesize - 1);
+    v.resize(pow(2, h + 1) + 1, 0);
+    int left = pow(2, h + 1) / 2 - 1;
+    
+    for (int i = 1; i <= N; i++) cin >> v[left + i];
+    init(pow(2, h + 1) - 1);
 
     for (int i = 0; i < M + K; i++) {
-        long a, s, e;
-        cin >> a >> s >> e;
-
-        if (a == 1) change(left + s, e);
-        else if (a == 2) {
-            s += left;
-            e += left;
-            cout << getsum(s, e) << endl;
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) { // b번째 수를 c로 변경
+            change(left + b, c);
+        } else if (a == 2) { // b번째부터 c번째까지 부분합 출력
+            cout << sum(left + b, left + c) << endl;
         }
     }
 
+    return 0;
 }
